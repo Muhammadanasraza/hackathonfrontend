812c84471df6d17efb92fc8e7da4fc79206354a8
Anas raza <154615716+Muhammadanasraza@users.noreply.github.com>
2025-01-26 04:31:55 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { BrowserRouter, Routes, Route } from "react-router";
 import './App.css'
 import Home from './Pages/Home';
+// import Dashboard from './Pages/Dashboard';
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
+          {/* <Route path="/dashboard" element={<Dashboard />} /> */}
         </Routes>
       </BrowserRouter>
     </>
