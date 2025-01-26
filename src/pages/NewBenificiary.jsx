d2e79e2f5265251bf2ff38f2b1923b6c8b35818d
Anas raza <154615716+Muhammadanasraza@users.noreply.github.com>
2025-01-27 01:02:12 +0500

    Update NewBenificiary.jsx

--- src/pages/NewBenificiary.jsx
+++ src/pages/NewBenificiary.jsx
@@ -30,30 +30,30 @@ function NewBeneficiary() {
     setQrCode(qrCodeUrl);
 
     // POST Data to the API
-    // setIsSubmitting(true);
-    // try {
-    //   const response = await fetch("http://localhost:4000/api/beneficiaries", {
-    //     method: "POST",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify(formData),
-    //   });
+    setIsSubmitting(true);
+    try {
+      const response = await fetch("http://localhost:4000/api/beneficiaries", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
 
-    //   if (response.ok) {
-    //     const data = await response.json();
-    //     alert("Beneficiary added successfully!");
-    //     console.log("Response:", data);
-    //   } else {
-    //     console.error("Failed to submit data:", response.statusText);
-    //     alert("Failed to add beneficiary. Please try again.");
-    //   }
-    // } catch (error) {
-    //   console.error("Error occurred:", error);
-    //   alert("An error occurred while submitting the form.");
-    // } finally {
-    //   setIsSubmitting(false);
-    // }
+      if (response.ok) {
+        const data = await response.json();
+        alert("Beneficiary added successfully!");
+        console.log("Response:", data);
+      } else {
+        console.error("Failed to submit data:", response.statusText);
+        alert("Failed to add beneficiary. Please try again.");
+      }
+    } catch (error) {
+      console.error("Error occurred:", error);
+      alert("An error occurred while submitting the form.");
+    } finally {
+      setIsSubmitting(false);
+    }
   };
 
   const downloadPdf = () => {
